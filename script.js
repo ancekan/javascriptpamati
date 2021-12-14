3482ed0ad86a51252f1b734be48d1bafaebd2ee7
Ance Kancere <ance.kancere@siguldaspv.edu.lv>
2021-12-14 16:10:11 +0200

    importeju projektu

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log('----------------------9.uzdevums - 1.variants (ar konstanti)------------------------------------')
+
+const kumulativais = 50
+
+if (kumulativais > 40.4) {
+    console.log('Jāievēro 10 dienu pašizolācija!')
+} else {
+    console.log('Nav jāievēro pašizolācija.')
+}
+
+console.log('----------------------9.uzdevums - 2.variants (ar aprēķiniem)------------------------------------')
+
+// const kumulativais = 50
+
+let iedzSk = 2000000;
+let saslimusie = 808;
+const kumulativaisMax = 40.4;
+let a = iedzSk/100000;
+let b = a*kumulativaisMax;
+
+if (saslimusie > b) {
+    console.log('Jāievēro 10 dienu pašizolācija!')
+} else {
+    console.log('Nav jāievēro pašizolācija.')
+}
+
+console.log('-------------------------8.uzdevums---------------------------------')
+
+const cars = ['Audi', 'Opel', 'Subaru', 'Volvo'];
+console.log(cars[3])
+
+console.log('-----------------------8.uzdevums-----------------------------------')
+
+const table = {
+    length: 100,
+    width: 70,
+    color: 'white'
+}
+
+console.log(table.color)
+
+
